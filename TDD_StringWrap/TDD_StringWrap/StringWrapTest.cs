aff115f6bbeff807fcc577b2253ae802f51c635d
Tarun <Tarun@DESKTOP-AFA40BG>
2018-02-01 13:13:45 -0800

    TestNullString - Green

---
+++ TDD_StringWrap/TDD_StringWrap/StringWrapTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_StringWrap
+{
+    [TestClass]
+    public class StringWrapTest
+    {
+        [TestMethod]
+        public void TestNullString()
+        {
+            String input = null, output = null, expected = null;
+            var colmax = 15;
+
+            StringManip ObjStringManip = new StringManip();
+
+            // Act
+            output = ObjStringManip.WrapMyString(input, colmax);
+
+            // Assert
+            Assert.AreEqual(expected, output);
+
+        }
+    }
+}

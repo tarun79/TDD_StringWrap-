476ab715471bf5b6a85430f119e2a0b8344ecbc2
Tarun <Tarun@DESKTOP-AFA40BG>
2018-02-01 13:09:04 -0800

    TestNullString - Red

---
+++ TDD_StringWrap/TDD_StringWrap/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_StringWrap
+{
+    [TestClass]
+    public class StringWrapTest
+    {
+        [TestMethod]
+        public void TestNullString()
+        {
+
+        }
+    }
+}

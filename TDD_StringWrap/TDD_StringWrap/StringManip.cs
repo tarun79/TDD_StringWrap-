f05c140b93ad30efa6a52463162fbee152d200fb
Tarun <Tarun@DESKTOP-AFA40BG>
2018-02-01 14:16:52 -0800

    TestSampleString - Green

--- TDD_StringWrap/TDD_StringWrap/StringManip.cs
+++ TDD_StringWrap/TDD_StringWrap/StringManip.cs
@@ -10,12 +10,37 @@ namespace TDD_StringWrap
     {
         public String WrapMyString(string input, int colmax)
         {
-            String outString = null;
-
             if (input == null)
                 return null;
-           
-            return outString;
+
+            StringBuilder outString = new StringBuilder();
+            StringBuilder row_string = new StringBuilder();
+            string[] wordsInString = input.Split(' ');
+
+            if (wordsInString[0].Length < colmax)
+                row_string.Append(wordsInString[0]);
+            else
+                return "Column size is too small";
+
+            for (var i = 1; i < wordsInString.Length; i++)
+            {
+                if (row_string.Length + wordsInString[i].Length <= colmax - 1)
+                {
+                    row_string.Append(" ");
+                    row_string.Append(wordsInString[i]);
+                }
+                else
+                {
+                    row_string.Append("\n");
+                    outString.Append(row_string);
+                    row_string.Clear();
+                    row_string.Append(wordsInString[i]);
+                }
+            }
+
+            outString.Append(row_string);
+
+            return outString.ToString(); 
         }
     }
 }
